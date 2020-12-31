2d5f330c9bf601bbc1de4bd645b4414f61eeeca8
Fabricio Caneva <hardlymersk@gmail.com>
2020-12-31 14:20:14 -0300

    initial

---
+++ src/main/java/me/caneva20/wayportals/WayPortals.java
@@ -0,0 +1,14 @@
+package me.caneva20.wayportals;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class WayPortals extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+    }
+
+    @Override
+    public void onDisable() {
+    }
+}
