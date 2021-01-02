3c4ce158b781f896151da756c727c22619812c7e
Fabricio Caneva <hardlymersk@gmail.com>
2021-01-02 18:22:09 -0300

    add guice

--- src/main/java/me/caneva20/wayportals/WayPortals.java
+++ src/main/java/me/caneva20/wayportals/WayPortals.java
@@ -1,11 +1,29 @@
 package me.caneva20.wayportals;
 
+import co.aikar.commands.PaperCommandManager;
+import me.caneva20.messagedispatcher.dispachers.IConsoleMessageDispatcher;
+import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import javax.inject.Inject;
+
 public final class WayPortals extends JavaPlugin {
+    @Inject
+    private IConsoleMessageDispatcher dispatcher;
+
+    @Inject
+    private PaperCommandManager commandManager;
+
+    @Inject
+    private PluginManager pluginManager;
 
     @Override
     public void onEnable() {
+        var injector = new BinderModule(this).createInjector();
+
+        injector.injectMembers(this);
+
+        dispatcher.success("Enabled! :)");
     }
 
     @Override
