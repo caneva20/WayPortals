5ef3e0e5ef163a81bdb31d89b8e195775bd150e7
Fabricio Caneva <hardlymersk@gmail.com>
2021-01-03 10:50:14 -0300

    add plugin's `NamespacedKey` service

---
+++ src/main/java/me/caneva20/wayportals/KeyProvider.java
@@ -0,0 +1,27 @@
+package me.caneva20.wayportals;
+
+import org.bukkit.NamespacedKey;
+
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
+@Singleton
+public class KeyProvider {
+    private static final String portalBinderStringKey = "portal_binder";
+    private static final String portalBinderTargetStringKey = "portal_binder_location";
+
+    private final WayPortals plugin;
+
+    @Inject
+    KeyProvider(WayPortals plugin) {
+        this.plugin = plugin;
+    }
+
+    public NamespacedKey getPortalBinderKey() {
+        return new NamespacedKey(plugin, portalBinderStringKey);
+    }
+
+    public NamespacedKey getPortalBinderTargetKey() {
+        return new NamespacedKey(plugin, portalBinderTargetStringKey);
+    }
+}
