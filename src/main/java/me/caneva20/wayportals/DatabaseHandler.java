93832585ac05b5014cde0f435e10ded0612e6f02
Fabricio Caneva <hardlymersk@gmail.com>
2021-01-06 00:29:12 -0300

    remove `id_string` from portals table

--- src/main/java/me/caneva20/wayportals/DatabaseHandler.java
+++ src/main/java/me/caneva20/wayportals/DatabaseHandler.java
@@ -44,8 +44,8 @@ public class DatabaseHandler {
 
   private void createDatabase() {
     try {
-      DB.executeUpdate("CREATE TABLE IF NOT EXISTS 'portals' ('id' INTEGER NOT NULL, 'id_string' TEXT NOT NULL UNIQUE, 'linked_portal_id' INTEGER, 'world' TEXT NOT NULL DEFAULT '', 'min_x' INTEGER NOT NULL DEFAULT 0, 'min_y' INTEGER NOT NULL DEFAULT 0, 'min_z' INTEGER NOT NULL DEFAULT 0, 'max_x' INTEGER NOT NULL DEFAULT 0, 'max_y' INTEGER NOT NULL DEFAULT 0, 'max_z' INTEGER NOT NULL DEFAULT 0, PRIMARY KEY('id' AUTOINCREMENT), FOREIGN KEY('linked_portal_id') REFERENCES 'portals'('id'))");
-      DB.executeUpdate("CREATE INDEX IF NOT EXISTS 'portal_id' ON 'portals'('id', 'id_string')");
+      DB.executeUpdate("CREATE TABLE IF NOT EXISTS 'portals' ('id' INTEGER NOT NULL, 'linked_portal_id' INTEGER, 'world' TEXT NOT NULL DEFAULT '', 'min_x' INTEGER NOT NULL DEFAULT 0, 'min_y' INTEGER NOT NULL DEFAULT 0, 'min_z' INTEGER NOT NULL DEFAULT 0, 'max_x' INTEGER NOT NULL DEFAULT 0, 'max_y' INTEGER NOT NULL DEFAULT 0, 'max_z' INTEGER NOT NULL DEFAULT 0, PRIMARY KEY('id' AUTOINCREMENT), FOREIGN KEY('linked_portal_id') REFERENCES 'portals'('id'))");
+      DB.executeUpdate("CREATE INDEX IF NOT EXISTS 'portal_id' ON 'portals'('id')");
     } catch (SQLException ex) {
       ex.printStackTrace();
     }
