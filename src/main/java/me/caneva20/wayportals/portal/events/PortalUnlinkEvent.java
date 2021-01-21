8298487ac376837b9d12542b748cd072bae5db9c
Fabricio Caneva <hardlymersk@gmail.com>
2021-01-20 22:59:21 -0300

    add lombok Getters

--- src/main/java/me/caneva20/wayportals/portal/events/PortalUnlinkEvent.java
+++ src/main/java/me/caneva20/wayportals/portal/events/PortalUnlinkEvent.java
@@ -1,11 +1,13 @@
 package me.caneva20.wayportals.portal.events;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import me.caneva20.wayportals.portal.Portal;
 import me.caneva20.wayportals.utils.events.BaseEvent;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+@Getter
 @RequiredArgsConstructor
 public class PortalUnlinkEvent extends BaseEvent {
 
